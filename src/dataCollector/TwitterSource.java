5762611d7b9a77a1d963c298d242578fb5449193
Jihad <jihadbaeth@gmail.com>
2015-11-29 20:19:28 +0200

    0.01

---
+++ src/dataCollector/TwitterSource.java
@@ -0,0 +1,80 @@
+package dataCollector;
+
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import twitter4j.FilterQuery;
+import twitter4j.StallWarning;
+import twitter4j.Status;
+import twitter4j.StatusDeletionNotice;
+import twitter4j.StatusListener;
+import twitter4j.TwitterException;
+import twitter4j.TwitterStream;
+import twitter4j.TwitterStreamFactory;
+import twitter4j.conf.ConfigurationBuilder;
+
+public class TwitterSource {
+	
+    
+    
+    public static void main(String[] args) throws TwitterException {
+    	
+    	
+    	NLP.init();
+
+    ConfigurationBuilder cb = new ConfigurationBuilder();
+    cb.setDebugEnabled(true);
+    cb.setOAuthConsumerKey("BlxW064mvxjGhpcIOaS42Ybbd");
+    cb.setOAuthConsumerSecret("U6Gz5n7635mFfbtqI1oxxKMCBR5QcP9niQUbAAU9D3mz0l3uTA");
+    cb.setOAuthAccessToken("1395669122-l4FA606uDOJ5sp3WUiEbzpmJ1fm2HqtBYBxSoPO");
+    cb.setOAuthAccessTokenSecret("6TLS69gSeh3XZPPxT4ulX7xWda6O4asGDB3B3gsLoWxv8");
+
+    TwitterStream twitterStream = new TwitterStreamFactory(cb.build()).getInstance();
+    StatusListener listener = new StatusListener() {
+
+        public void onStatus(Status status) {
+        	System.out.println("****** New Tweet Incoming ******");
+            System.out.println("@" + status.getUser().getScreenName() + " - " + status.getText());
+            System.out.println("Created At "+status.getCreatedAt());
+            System.out.println("Being favoried by "+status.getFavoriteCount()+" users");
+            System.out.println("get place "+status.getPlace());
+            System.out.println("Follwoing/Followed By ratio is: "+status.getUser().getFollowersCount()/status.getUser().getFriendsCount());
+            System.out.println("User verification status is: "+status.getUser().isVerified());
+            System.out.println("Get Lang: "+status.getLang());
+            
+            System.out.println("The sentement Analysis Score is:"+ NLP.findSentiment(status.getText()));
+        	System.out.println("****** End of Tweet Stream ******");
+
+        }
+
+        public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {
+            System.out.println("Got a status deletion notice id:" + statusDeletionNotice.getStatusId());
+        }
+
+        public void onTrackLimitationNotice(int numberOfLimitedStatuses) {
+            System.out.println("Got track limitation notice:" + numberOfLimitedStatuses);
+        }
+
+        public void onScrubGeo(long userId, long upToStatusId) {
+            System.out.println("Got scrub_geo event userId:" + userId + " upToStatusId:" + upToStatusId);
+        }
+
+        public void onException(Exception ex) {
+            ex.printStackTrace();
+        }
+
+		@Override
+		public void onStallWarning(StallWarning arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+    };
+
+    FilterQuery fq = new FilterQuery();
+    String keywords[] = {"happy"};
+
+    fq.track(keywords);
+
+    twitterStream.addListener(listener);
+    twitterStream.filter(fq);      
+}
+}
---
+++ src/dataCollector/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author jihad
+ *
+ */
+package dataCollector;
